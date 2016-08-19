28038f1b137ed6760fee455b52411c30c6f36758
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 14:48:12 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 {
     'name': "Supplier pricelists reorderer",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['product',],
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
